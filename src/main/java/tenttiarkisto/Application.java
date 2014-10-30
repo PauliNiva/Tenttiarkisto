9b2fdb6aacf2af0712ac180b7497f3d586cee088
Topias Heinonen <corvus@iki.fi>
2014-10-30 20:30:47 +0200

    Hello World!

--- src/main/java/tenttiarkisto/Application.java
+++ src/main/java/tenttiarkisto/Application.java
@@ -4,23 +4,13 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-@RestController
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
-
-    @RequestMapping("/")
-    @ResponseBody
-    String home() {
-        return "Hello World!";
     }
 }
---
+++ src/main/java/tenttiarkisto/controllers/DefaultController.java
@@ -0,0 +1,15 @@
+
+package tenttiarkisto.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DefaultController {
+    
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+    
+}
