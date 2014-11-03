c90191154cc411e41ad76f829bff4408d3f036cd
Topias Heinonen <corvus@iki.fi>
2014-11-03 23:33:09 +0200

    Ainakin yksi view toimii

--- src/main/java/tenttiarkisto/Application.java
+++ src/main/java/tenttiarkisto/Application.java
@@ -1,14 +1,21 @@
 package tenttiarkisto;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/tenttiarkisto/controllers/DefaultController.java
+++ src/main/java/tenttiarkisto/controllers/DefaultController.java
@@ -1,17 +1,17 @@
-
 package tenttiarkisto.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DefaultController {
+
     
     @RequestMapping("/")
     public String view(Model model) {
-	model.addAttribute("viesti", "gagaeeagf");
+        model.addAttribute("viesti", "Viestitellään");
         return "WEB-INF/views/index.jsp";
     }
-    
+
 }
