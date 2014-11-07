0dd26b54e31f5d0313ab16ed102053b55fa05678
Topias Heinonen <corvus@iki.fi>
2014-11-07 03:45:18 +0200

    S3 service lisätty

---
+++ src/main/java/tenttiarkisto/service/FileService.java
@@ -0,0 +1,8 @@
+package tenttiarkisto.service;
+
+import java.io.File;
+
+public interface FileService {
+    //returns URL to the file
+    public String putFile(File f);
+}
---
+++ src/main/java/tenttiarkisto/service/S3FileService.java
@@ -0,0 +1,60 @@
+package tenttiarkisto.service;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import java.io.File;
+import org.apache.log4j.Logger;
+import org.apache.log4j.Priority;
+
+public class S3FileService implements FileService {
+
+    private final AmazonS3 s3;
+    private final Region ireland;
+    private final String bucket;
+
+    public S3FileService() {
+        s3 = new AmazonS3Client();
+        ireland = Region.getRegion(Regions.EU_WEST_1);
+        s3.setRegion(ireland);
+
+        bucket = "tentit";
+    }
+
+    @Override
+    public String putFile(File f) {
+        try {
+            PutObjectRequest req = new PutObjectRequest(bucket, f.getName(), f);
+            s3.putObject(req);
+
+            return "https://s3-eu-west-1.amazonaws.com/tentit/" + f.getName();
+        } catch (AmazonServiceException ase) {
+            String msg = "Caught an AmazonServiceException, which means your request made it "
+                    + "to Amazon S3, but was rejected with an error response for some reason.";
+            msg += "\nError Message:    " + ase.getMessage();
+            msg += "\nHTTP Status Code: " + ase.getStatusCode();
+            msg += "\nAWS Error Code:   " + ase.getErrorCode();
+            msg += "\nError Type:       " + ase.getErrorType();
+            msg += "\nRequest ID:       " + ase.getRequestId();
+            
+            Logger.getLogger(S3FileService.class).log(Priority.ERROR, msg);
+            
+            return null;
+        } catch (AmazonClientException ace) {
+            String msg = "Caught an AmazonClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with S3, "
+                    + "such as not being able to access the network.";
+            msg += "\nError Message: " + ace.getMessage();
+
+            Logger.getLogger(S3FileService.class).log(Priority.ERROR, msg);
+
+            return null;
+        }
+
+    }
+
+}
