009acbe34d624c4be881fa2cc25bf74745e9ecc4
Pauli Niva <pauli.niva@helsinki.fi>
2014-11-16 14:36:59 +0200

    Lisätty toiminnallisuuksia
    Autentikointia, admin (person) olio, repo ja palvelu sille sekä
    tietoturvakonffi

---
+++ src/main/java/tenttiarkisto/repo/PersonRepo.java
@@ -0,0 +1,8 @@
+package tenttiarkisto.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tenttiarkisto.domain.Person;
+
+public interface PersonRepo extends JpaRepository<Person, Long> {
+    Person findByUsername(String username);   
+}
